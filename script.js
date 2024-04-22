ad1495dc904d10882d7462b903ee1ce85edd30b9
Harishgopal17 <harishgopal1705@gmail.com>
2024-04-22 17:40:05 +0530

    working on animations

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const btn = document.querySelector(".activity_btn");
 const btn_close = document.querySelector(".btn--close-modal");
 const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
+const btn_scroll = document.querySelector(".btn_scroll_to");
+const section1 = document.querySelector(".section1");
+const header = document.querySelector(".header");
+const logo = document.querySelector(".nav_logo");
+const routine_break = document.querySelector(".routine_break_section");
 
 btn.addEventListener("click", function () {
   modal.classList.toggle("visibility_hidden");
@@ -20,3 +25,25 @@ document.addEventListener("keydown", function (e) {
     btn.blur();
   }
 });
+
+btn_scroll.addEventListener("click", function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+});
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    console.log("Intersection observer entries:", entries);
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        routine_break.classList.toggle("section--hidden");
+      }
+      // if (!entry.isIntersecting) observer.unobserve(entry);
+    });
+  },
+  {
+    root: null,
+    threshold: 0.1,
+  }
+);
+
+observer.observe(routine_break);
